2320cfe9175ed02fec30c3f50f6b62cad4cb14ad
Dhruv Makwana <dc_mak@outlook.com>
2022-12-14 22:39:04 +0000

    Apply headache

--- etc/clear_headache.sh
+++ etc/clear_headache.sh
@@ -5,5 +5,5 @@ headache -c etc/headache_config -r \
     .gitignore .travis.yml README.md cmake/FindLLVM.cmake src/CMakeLists.txt \
     `find src -name '*.cpp'` \
     `find src -name '*.hpp' ! -path 'src/debug.hpp'` \
-    `find test -name '.py'` \
+    `find test -name '*.py'` \
     src/debug.hpp
