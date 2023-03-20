c1d60143f362452abffa929fca75de6daf7aa36d
Dhruv Makwana <dc_mak@outlook.com>
2023-03-20 17:46:42 +0000

    Remove hard-coded directory
    This file is to be able to run individual tests from the command-line

--- test/make_compile_commands.sh
+++ test/make_compile_commands.sh
@@ -5,11 +5,13 @@
 
 set -euo pipefail
 
+DIRECTORY=$(dirname $PWD)
+
 echo "[" > compile_commands.json
 
 for input_file in cxx_record/*.in.cpp  function/*.in.cpp  record/*.in.cpp  simple/*.in.cpp typedef/*.in.cpp; do
   echo '    {' >> compile_commands.json
-  echo '        directory: "/home/dhruv/c-simplifier",' >> compile_commands.json
+  echo "        directory: \"$DIRECTORY\"," >> compile_commands.json
   echo "        command: \"clang -c test/$input_file\"," >> compile_commands.json
   echo "        file: \"test/$input_file\"" >> compile_commands.json
   echo '    },' >> compile_commands.json
