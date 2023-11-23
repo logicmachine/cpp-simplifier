de0a80fa0c9636258150ebef05de2de3813604fa
Dhruv Makwana <dc_mak@outlook.com>
2023-11-23 15:19:21 +0000

    Adjust struct_fields.c test

--- cpp/test/to-add/struct_fields.h
+++ cpp/test/to-add/struct_fields.h
@@ -1,23 +1,33 @@
-struct s1 { int s1_field; };
+struct s1 {
+    int s1_field;
+    char UNUSED;
+};
 
-typedef struct { int s2_field; } s2;
+typedef struct {
+    int s2_field;
+} s2;
 
-enum e1 { ONE=5, TWO=(7 << (2-1)) };
+enum e1 {
+    ONE=5,
+    TWO=(7 << (2-1))
+};
 
-// typedef struct {
-//  int s3_field;
-// } __attribute__((__aligned__((1 << (6))))) s3;
 typedef struct {
  int s3_field;
-} s3;
+} __attribute__((__aligned__((1 << (6))))) s3;
+/* multiline
+   block
+   comment */
 
-extern struct s2 thing2;
+extern struct s2 thing2; /* single-line block comment */
 
-// extern __attribute__((section(".data..percpu" "..shared_aligned"))) __typeof__(s3) irq_stat __attribute__((__aligned__((1 << (6)))));
-extern s3 irq_stat;
+extern __attribute__((section(".data..percpu" "..shared_aligned"))) __typeof__(s3) irq_stat __attribute__((__aligned__((1 << (6)))));
+
 
 typedef struct {
     s2 (*s4_field)(s3);
 } s4;
 
-enum e5 { THREE=3 };
+enum e5 {
+    THREE=3
+};
